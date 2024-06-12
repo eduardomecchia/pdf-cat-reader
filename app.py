148aae460350fff5e64be1ccb2eb6bee59423524
Eduardo Mecchia <eduardomecchia273@gmail.com>
2024-06-13 00:30:31 +0200

    feat: count total words

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ st.title('Hi! Welcome to the PDF Cat Reader.')
 
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf", accept_multiple_files=False)
 
-
-
 if uploaded_file is not None:
-    #process_pdf(uploaded_file)
-    doc = pymupdf.open(uploaded_file)
+    doc = pymupdf.open(stream=uploaded_file.read(), filetype="pdf")
+
+    words_count = 0
+    for page in doc:
+        words_count += len(page.get_text().split())
+
+    st.write(f"Total words in the PDF: {words_count}")
