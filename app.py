2107d1c65c543b667b47d33b054821e6f04ac531
Eduardo Mecchia <eduardomecchia273@gmail.com>
2024-06-12 23:50:13 +0200

    feat: try using PyMuPDF to open documents

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import fitz
 
 st.title('Octostar Cat')
+
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf", accept_multiple_files=False)
+
+
+
+if uploaded_file is not None:
+    #process_pdf(uploaded_file)
+    doc = fitz.open(uploaded_file)
